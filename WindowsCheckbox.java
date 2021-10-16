f2f1a125977c714b2f19a55b06e00e70d712df20
sdadige <90069155+sdadige@users.noreply.github.com>
2021-10-16 08:51:04 +0530

    Create WindowsCheckbox.java

---
+++ WindowsCheckbox.java
@@ -0,0 +1,8 @@
+//This is a Windows variant of a checkbox.
+public class WindowsCheckbox implements Checkbox {
+
+    @Override
+    public void paint() {
+        System.out.println("You have created WindowsCheckbox.");
+    }
+}
