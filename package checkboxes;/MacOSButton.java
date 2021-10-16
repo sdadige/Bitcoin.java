65b69c68e4c04a6be2dd829b1b98c4a1edc213e6
sdadige <90069155+sdadige@users.noreply.github.com>
2021-10-16 08:49:58 +0530

    Create MacOSButton.java

---
+++ package checkboxes;/MacOSButton.java
@@ -0,0 +1,11 @@
+package checkboxes;
+
+//This is a MacOS variant of a checkbox.
+public class MacOSCheckbox 
+    implements Checkbox {
+
+    @Override
+    public void paint() {
+        System.out.println("You have created MacOSCheckbox.");
+    }
+}
