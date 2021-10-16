ee2666120c52131c84270057a24864fe46e8eda1
sdadige <90069155+sdadige@users.noreply.github.com>
2021-10-16 08:48:45 +0530

    Create WindowsButton.java

---
+++ WindowsButton.java
@@ -0,0 +1,10 @@
+package bitcoin;
+
+//This is windows variant of a bitcoin.
+public class WindowsBitcoin implements Bitcoin {
+
+    @Override
+    public void paint() {
+        System.out.println("You have created WindowsBitcoin.");
+    }
+}
