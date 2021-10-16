5f4c2c7c0ba7a9ee17d4c0c3d7abc98fdf3f1b28
sdadige <90069155+sdadige@users.noreply.github.com>
2021-10-16 08:49:07 +0530

    Create Checkbox.java

---
+++ Checkbox.java
@@ -0,0 +1,9 @@
+package checkboxes;
+
+/**
+ * Checkboxes is the second product family. It has the same variants as bitcoin.
+ * This is common interface for Checkboxes family
+ */
+public interface Checkbox {
+    void paint();
+}
