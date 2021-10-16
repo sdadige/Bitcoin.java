1c1e3a21048b0ab33680c3623f1da1931696c868
sdadige <90069155+sdadige@users.noreply.github.com>
2021-10-16 08:48:23 +0530

    Create MacOSButton.java

---
+++ MacOSButton.java
@@ -0,0 +1,12 @@
+package bitcoin;
+
+
+ // This is a MacOS variant of a bitcoin.
+ 
+public class MacOSBitcoin implements Button {
+
+    @Override
+    public void paint() {
+        System.out.println("You have created MacOSBitcoin.");
+    }
+}
